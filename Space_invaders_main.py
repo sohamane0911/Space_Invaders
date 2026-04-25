67321e7074d0c7aa33babdf59cd2ea3113721f2c
sohamane0911 <sohamane0911@gmail.com>
2026-04-25 21:20:09 +0530

    Update Space_invaders_main.py
    added background and main menu assets

--- Space_invaders_main.py
+++ Space_invaders_main.py
@@ -1,15 +1,23 @@
 import pygame
 
 pygame.init()
+pygame.mixer.init()
 
 clock = pygame.time.Clock()
 FPS = 60
 
-# Game window setup (same as main code)
 screen_width = 1534
 screen_height = 900
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Space Invaders")
+
+main_menu_bg = pygame.transform.scale(
+    pygame.image.load("SI ain menu.png").convert(),
+    (1534, 900)
+)
+
+bg = pygame.image.load("bg moving.png").convert()
+bg_height = bg.get_height()
 
 running = True
 
@@ -20,4 +28,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    screen.blit(main_menu_bg, (0, 0))
+
     pygame.display.update()
+
+pygame.quit()
